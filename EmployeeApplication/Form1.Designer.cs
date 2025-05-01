3575968fbb01aaa648aef6ab1349c611b62499e4
emmanetan <emmannate283@gmail.com>
2025-05-01 12:53:31 +0800

    Add project files.

---
+++ EmployeeApplication/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace EmployeeApplication
+{
+    partial class frmEmployeeDatabase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAddEmployee = new System.Windows.Forms.Label();
+            this.lblEmployeeID = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.lblEmployeeList = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.txtFirstName = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.txtLastName = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.txtPosition = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.btnSubmit = new Siticone.UI.WinForms.SiticoneButton();
+            this.SuspendLayout();
+            // 
+            // lblAddEmployee
+            // 
+            this.lblAddEmployee.AutoSize = true;
+            this.lblAddEmployee.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddEmployee.Location = new System.Drawing.Point(19, 20);
+            this.lblAddEmployee.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAddEmployee.Name = "lblAddEmployee";
+            this.lblAddEmployee.Size = new System.Drawing.Size(155, 26);
+            this.lblAddEmployee.TabIndex = 0;
+            this.lblAddEmployee.Text = "Add Employee";
+            // 
+            // lblEmployeeID
+            // 
+            this.lblEmployeeID.AutoSize = true;
+            this.lblEmployeeID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmployeeID.Location = new System.Drawing.Point(19, 57);
+            this.lblEmployeeID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmployeeID.Name = "lblEmployeeID";
+            this.lblEmployeeID.Size = new System.Drawing.Size(110, 20);
+            this.lblEmployeeID.TabIndex = 1;
+            this.lblEmployeeID.Text = "Employee ID *";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(19, 122);
+            this.lblFirstName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(94, 20);
+            this.lblFirstName.TabIndex = 2;
+            this.lblFirstName.Text = "First name *";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(19, 187);
+            this.lblLastName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(94, 20);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Last name *";
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPosition.Location = new System.Drawing.Point(19, 252);
+            this.lblPosition.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(75, 20);
+            this.lblPosition.TabIndex = 4;
+            this.lblPosition.Text = "Position *";
+            // 
+            // lblEmployeeList
+            // 
+            this.lblEmployeeList.AutoSize = true;
+            this.lblEmployeeList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmployeeList.Location = new System.Drawing.Point(244, 24);
+            this.lblEmployeeList.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmployeeList.Name = "lblEmployeeList";
+            this.lblEmployeeList.Size = new System.Drawing.Size(108, 20);
+            this.lblEmployeeList.TabIndex = 5;
+            this.lblEmployeeList.Text = "Employee List";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtEmployeeID.DefaultText = "";
+            this.txtEmployeeID.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtEmployeeID.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtEmployeeID.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtEmployeeID.DisabledState.Parent = this.txtEmployeeID;
+            this.txtEmployeeID.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtEmployeeID.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtEmployeeID.FocusedState.Parent = this.txtEmployeeID;
+            this.txtEmployeeID.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtEmployeeID.HoveredState.Parent = this.txtEmployeeID;
+            this.txtEmployeeID.Location = new System.Drawing.Point(23, 80);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.PasswordChar = '\0';
+            this.txtEmployeeID.PlaceholderText = "";
+            this.txtEmployeeID.SelectedText = "";
+            this.txtEmployeeID.ShadowDecoration.Parent = this.txtEmployeeID;
+            this.txtEmployeeID.Size = new System.Drawing.Size(186, 34);
+            this.txtEmployeeID.TabIndex = 12;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtFirstName.DefaultText = "";
+            this.txtFirstName.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtFirstName.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtFirstName.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFirstName.DisabledState.Parent = this.txtFirstName;
+            this.txtFirstName.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFirstName.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFirstName.FocusedState.Parent = this.txtFirstName;
+            this.txtFirstName.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFirstName.HoveredState.Parent = this.txtFirstName;
+            this.txtFirstName.Location = new System.Drawing.Point(23, 146);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.PasswordChar = '\0';
+            this.txtFirstName.PlaceholderText = "";
+            this.txtFirstName.SelectedText = "";
+            this.txtFirstName.ShadowDecoration.Parent = this.txtFirstName;
+            this.txtFirstName.Size = new System.Drawing.Size(186, 34);
+            this.txtFirstName.TabIndex = 13;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtLastName.DefaultText = "";
+            this.txtLastName.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtLastName.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtLastName.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtLastName.DisabledState.Parent = this.txtLastName;
+            this.txtLastName.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtLastName.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtLastName.FocusedState.Parent = this.txtLastName;
+            this.txtLastName.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtLastName.HoveredState.Parent = this.txtLastName;
+            this.txtLastName.Location = new System.Drawing.Point(23, 211);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.PasswordChar = '\0';
+            this.txtLastName.PlaceholderText = "";
+            this.txtLastName.SelectedText = "";
+            this.txtLastName.ShadowDecoration.Parent = this.txtLastName;
+            this.txtLastName.Size = new System.Drawing.Size(186, 34);
+            this.txtLastName.TabIndex = 14;
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPosition.DefaultText = "";
+            this.txtPosition.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtPosition.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtPosition.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPosition.DisabledState.Parent = this.txtPosition;
+            this.txtPosition.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPosition.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPosition.FocusedState.Parent = this.txtPosition;
+            this.txtPosition.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPosition.HoveredState.Parent = this.txtPosition;
+            this.txtPosition.Location = new System.Drawing.Point(24, 275);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.PasswordChar = '\0';
+            this.txtPosition.PlaceholderText = "";
+            this.txtPosition.SelectedText = "";
+            this.txtPosition.ShadowDecoration.Parent = this.txtPosition;
+            this.txtPosition.Size = new System.Drawing.Size(186, 34);
+            this.txtPosition.TabIndex = 15;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.CheckedState.Parent = this.btnSubmit;
+            this.btnSubmit.CustomImages.Parent = this.btnSubmit;
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.HoveredState.Parent = this.btnSubmit;
+            this.btnSubmit.Location = new System.Drawing.Point(62, 322);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.ShadowDecoration.Parent = this.btnSubmit;
+            this.btnSubmit.Size = new System.Drawing.Size(112, 32);
+            this.btnSubmit.TabIndex = 16;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // frmEmployeeDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(696, 389);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.lblEmployeeList);
+            this.Controls.Add(this.lblPosition);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblEmployeeID);
+            this.Controls.Add(this.lblAddEmployee);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmEmployeeDatabase";
+            this.Text = "Employee Database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAddEmployee;
+        private System.Windows.Forms.Label lblEmployeeID;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.Label lblEmployeeList;
+        private Siticone.UI.WinForms.SiticoneTextBox txtEmployeeID;
+        private Siticone.UI.WinForms.SiticoneTextBox txtFirstName;
+        private Siticone.UI.WinForms.SiticoneTextBox txtLastName;
+        private Siticone.UI.WinForms.SiticoneTextBox txtPosition;
+        private Siticone.UI.WinForms.SiticoneButton btnSubmit;
+    }
+}
+
---
+++ EmployeeApplication/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using EmployeeNamespace;
+using Siticone.UI.WinForms;
+
+namespace EmployeeApplication
+{
+    public partial class frmEmployeeDatabase : Form
+    {
+        private DataTable table;
+        private SiticoneDataGridView dgridView;
+
+        public frmEmployeeDatabase()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+        }
+
+        public void InitializeDataGridView()
+        {
+            //dataTable
+            table = new DataTable();
+            table.Columns.Add("id", typeof(long));
+            table.Columns.Add("firstName", typeof(string));
+            table.Columns.Add("lastName", typeof(string));
+            table.Columns.Add("position", typeof(string));
+
+            //layout 
+            dgridView = new SiticoneDataGridView
+            {
+                //Dock = DockStyle.Fill,
+                ThemeStyle =
+                {
+                    AlternatingRowsStyle =
+                    {
+                        BackColor = Color.FromArgb(238, 239, 249)
+                    },
+                    RowsStyle =
+                    {
+                        BackColor = Color.White,
+                        ForeColor = Color.FromArgb(71, 69, 94),
+                        SelectionBackColor = Color.FromArgb(231, 229, 255),
+                        SelectionForeColor = Color.FromArgb(71, 69, 94)
+                    },
+
+                    HeaderStyle =
+                    {
+                        BackColor = Color.FromArgb(94, 148, 255),
+     
+                    }
+                },
+                GridColor = Color.FromArgb(94, 148, 255),
+                ReadOnly = true,
+                Size = new Size(421, 274),
+                Location = new Point(248, 80),
+                Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right,
+                AutoGenerateColumns = true          
+            };
+
+            // binding the datatable to the datagridview
+            dgridView.DataSource = table;
+            this.Controls.Add(dgridView);
+        }
+
+        private void ClearFields()
+        {
+            txtEmployeeID.Clear();
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtPosition.Clear();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (string.IsNullOrWhiteSpace(txtEmployeeID.Text) ||
+                    string.IsNullOrWhiteSpace(txtFirstName.Text) ||
+                    string.IsNullOrWhiteSpace(txtLastName.Text) ||
+                    string.IsNullOrWhiteSpace(txtPosition.Text))
+                {
+                    MessageBox.Show("All fields are required. Please fill in all fields.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                
+                if (!long.TryParse(txtEmployeeID.Text, out long employeeID))
+                {
+                    MessageBox.Show("Invalid Employee ID. Please enter a valid numeric value.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string firstName = txtFirstName.Text;
+                string lastName = txtLastName.Text;
+                string position = txtPosition.Text;
+
+                //creating an employee object
+                Employee employee = new Employee(employeeID, firstName, lastName, position);
+
+                //add the employee data to the dataTtable
+                table.Rows.Add(employee.EmployeeID, employee.FirstName, employee.LastName, employee.Position);
+
+                //clear input
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
